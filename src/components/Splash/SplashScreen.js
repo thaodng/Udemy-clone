59aa7ce083c17f04dba5a4482b5c39510b7ee735
Duy Nguyen <ngduythao2805@outlook.com>
2020-05-18 15:46:28 +0700

    Spalsh screen - don't really need in expo

---
+++ src/components/Splash/SplashScreen.js
@@ -0,0 +1,22 @@
+/* By using expo, we don't need this component, add splash in app.json */
+import React, { useEffect } from 'react';
+import { StyleSheet, ImageBackground } from 'react-native';
+import SplashImage from '../../assets/images/splash.png';
+
+const SplashScreen = ({ navigation }) => {
+  useEffect(() => {
+    setTimeout(() => {
+      navigation.navigate('SwiperScreen');
+    }, 3000);
+  }, []);
+
+  return <ImageBackground style={style.bg} source={SplashImage} />;
+};
+
+export default SplashScreen;
+
+const style = StyleSheet.create({
+  bg: {
+    flex: 1,
+  },
+});
